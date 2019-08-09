e10742658ad07e2d0e4f2f07670d95ce07035e1b
Abhi Aiyer <abhiaiyer91@gmail.com>
2019-08-08 21:39:20 -0700

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,6 +20,7 @@ const About = ({ data: { about } }) => (
             __html: about.bioNode.childMarkdownRemark.html,
           }}
         />
+        <p>suh dude</p>
       </div>
     </article>
   </Layout>
